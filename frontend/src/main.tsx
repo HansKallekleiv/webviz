ab16c12f200585b2a54c2416ff41bb82f4399e7f
Hans Kallekleiv <16436291+HansKallekleiv@users.noreply.github.com>
2023-11-22 11:56:08 +0100

    Try insight in frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,10 +3,30 @@ import { createRoot } from "react-dom/client";
 
 import { AuthProvider } from "@framework/internal/providers/AuthProvider";
 import { CustomQueryClientProvider } from "@framework/internal/providers/QueryClientProvider";
+import { ReactPlugin } from "@microsoft/applicationinsights-react-js";
+import { ApplicationInsights } from "@microsoft/applicationinsights-web";
+
+import { createBrowserHistory } from "history";
 
 import App from "./App";
 import { GlobalErrorBoundary } from "./GlobalErrorBoundary";
 
+if (import.meta.env.VITE_APPLICATIONINSIGHTS_CONNECTION_STRING) {
+    console.log("it works");
+    const browserHistory = createBrowserHistory();
+    const reactPlugin = new ReactPlugin();
+    const appInsights = new ApplicationInsights({
+        config: {
+            instrumentationKey: import.meta.env.VITE_APPLICATIONINSIGHTS_CONNECTION_STRING,
+            extensions: [reactPlugin],
+            extensionConfig: {
+                [reactPlugin.identifier]: { history: browserHistory },
+            },
+        },
+    });
+    appInsights.loadAppInsights();
+    appInsights.trackPageView();
+}
 /*
     If the `cleanStart` query parameter is given, 
     the application will clear all local storage before rendering the application.
