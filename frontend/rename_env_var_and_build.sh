c6b56263d96de6842634ef9619a63b9dfcf277f2
Hans Kallekleiv <16436291+HansKallekleiv@users.noreply.github.com>
2023-11-22 13:54:35 +0100

    script

--- frontend/rename_env_var_and_build.sh
+++ frontend/rename_env_var_and_build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 export VITE_APPLICATIONINSIGHTS_CONNECTION_STRING=$APPLICATIONINSIGHTS_CONNECTION_STRING
 
-exec npm ci --include=dev && npm run build && node compress_static.cjs
+exec "npm ci --include=dev && npm run build && node compress_static.cjs"
 
